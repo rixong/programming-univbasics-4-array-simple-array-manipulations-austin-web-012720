3dd21a802525c736c73d6567a0492d0e0aac01b5
Rick Glascock <rixong@gmail.com>
2019-10-17 13:39:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,13 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-using_insert(array,element)
-array.insert(element,3)
+def using_insert(array,element)
+  array.insert(3,element)
+end
+
+using_uniq (array)
+  
+end
+
+
 
